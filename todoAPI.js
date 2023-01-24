d062ae53cda89fca7c4fb4bde1ecd26dd9c05e19
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-01-25 00:52:02 +0530

    chore: refactor code and add failure status codes

--- todoAPI.js
+++ todoAPI.js
@@ -1,5 +1,4 @@
 const http = require('http');
-const qs = require('qs');
 
 const host = 'localhost';
 const port = 8000;
@@ -34,21 +33,23 @@ const todoListener = (req, res) => {
           res.write(JSON.stringify(foundTodo));
           res.end();
         } else {
+          res.writeHead(404, 'Not Found');
           res.write('Todo not found!');
         }
       } else {
+        res.writeHead(404, 'Not Found');
         res.write('Todo not found!');
         res.end();
       }
     }
-  } else if (reqMethod === 'POST') {
+  } else if (reqMethod === 'POST' && req.url === '/todos') {
     let data = '';
     req.on('data', (chunk) => {
       data += chunk;
     });
     req.on('end', _ => {
       let dataObj = JSON.parse(data);
-      dataObj['isCompleted'] = false;
+      dataObj['isComplete'] = false;
       dataObj["id"] = todos.length + 1;
       todos.push(dataObj);
     });
@@ -56,42 +57,44 @@ const todoListener = (req, res) => {
     res.write('Created new Task', 'utf-8', null);
     res.end();
 
-  } else if (reqMethod === 'DELETE') {
-    if (reqUrl === '/todos') {
-      todos = todos.filter((todo) => todo.isComplete === false);
-      res.write('Successfully deleted completed todos.');
+  } else if (reqMethod === 'DELETE' && reqUrl === '/todos') {
+    todos = todos.filter((todo) => todo.isComplete === false);
+    res.write('Successfully deleted completed todos.');
+    res.end();
+  } else if (reqMethod === 'PATCH' && reqUrl.startsWith('/todos/')) {
+    const todoId = Number(reqUrl.substring(7));
+    if (todoId) {
+      let data = '';
+      req.on('data', (chunk) => { data += chunk; });
+      req.on('end', _ => {
+        let dataObj = JSON.parse(data);
+        let foundTodo = todos.find((todo) => todo['id'] === todoId);
+        found = foundTodo;
+        Object.assign(foundTodo, dataObj);
+      });
+      res.writeHead(204, 'Successfully PATCHED');
       res.end();
+    } else {
+      res.writeHead(404);
+      res.end('Not Found');
     }
-  } else if (reqMethod === 'PATCH') {
-    if (reqUrl.startsWith('/todos/')) {
-      const todoId = Number(reqUrl.substring(7));
-      if (todoId) {
-        let data = '';
-        req.on('data', (chunk) => { data += chunk; });
-        req.on('end', _ => {
-          let dataObj = JSON.parse(data);
-          let foundTodo = todos.find((todo) => todo['id'] === todoId);
-          found = foundTodo;
-          Object.assign(foundTodo, dataObj);
-        });
-        res.writeHead(200, 'Successfully PATCHED');
-        res.end();
-      }
-    }
-  } else if (reqMethod === 'PUT') {
-    if (reqUrl.startsWith('/todos/')) {
-      const todoId = reqUrl.substring(7);
-      if (todoId) {
-        let data = '';
-        req.on('data', (chunk) => { data += chunk; });
-        req.on('end', _ => {
-          let dataObj = JSON.parse(data);
-          todos.splice(todoId - 1, 0, dataObj);
-          todos.splice(todoId, 1);
-        });
-        res.writeHead(204, 'No Content');
-        res.end('Successfully PUT');
-      }
+  } else if (reqMethod === 'PUT' && reqUrl.startsWith('/todos/')) {
+    const todoId = reqUrl.substring(7);
+    if (todoId) {
+      let data = '';
+      req.on('data', (chunk) => { data += chunk; });
+      req.on('end', _ => {
+        let dataObj = JSON.parse(data);
+        dataObj.isComplete = false;
+        dataObj.id = todos.length;
+        todos.splice(todoId - 1, 0, dataObj);
+        todos.splice(todoId, 1);
+      });
+      res.writeHead(204, 'No Content');
+      res.end('Successfully PUT');
+    } else {
+      res.writeHead(404);
+      res.end('Not Found');
     }
   }
   res.end();

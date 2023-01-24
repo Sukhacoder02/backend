7e06186a7f9fe37736f0c6abdc3c9f3c77cf4115
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-01-24 14:27:15 +0530

    feat: implement GET method with users

--- index.js
+++ index.js
@@ -23,7 +23,30 @@ const users = [
 const getUsers = (req, res) => {
   let url = req.url;
   res.writeHead(200);
-  res.end('Hello World!');
+  if (url === '/users') {
+    res.write(JSON.stringify(users, null, 2));
+    res.end();
+  } else if (url.startsWith('/users/')) {
+    let idStr = url.substring(7);
+    let idNum = Number(idStr);
+    if (!isNaN(idNum)) {
+      let foundUser = users.find((user) => user['id'] === idNum);
+      if (foundUser) {
+        res.write(JSON.stringify(foundUser));
+        res.end();
+      } else {
+        res.writeHead(404);
+        res.write('User not found!');
+        res.end();
+      }
+    } else {
+      res.writeHead(404);
+      res.write('User not found!');
+    }
+  }
+  else {
+    res.end('Hello World!');
+  }
 };
 
 

071055b65de032f988d6c12cabdbc9c6d13fee46
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-01-26 23:38:59 +0530

    chore: refactor code in requests handlers

--- index.js
+++ index.js
@@ -2,23 +2,6 @@ const http = require('http');
 
 const host = 'localhost';
 const port = '8000';
-const users = [
-  {
-    'name': 'Sukhman',
-    'FMNO': '328713',
-    'id': 1,
-  },
-  {
-    'name': 'Harbir',
-    'FMNO': '328747',
-    'id': 2,
-  },
-  {
-    'name': 'John Doe',
-    'FMNO': '123456',
-    'id': 3
-  }
-];
 
 const getUsers = (req, res) => {
   let url = req.url;
